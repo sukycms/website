872457dd36794ef2db6a2fa23aa888b183e1a1fe
Jaime Sares <jaimesares@gmail.com>
2020-06-08 19:53:56 +0200

    wip

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -2,12 +2,20 @@
 
 @section('content')
 
-    <div class="container mx-auto my-12">
-        <h1 class="text-3xl font-semibold">
-            {{ $post->title }}
-        </h1>
+    <div class="blog container mx-auto my-12">
+        <div class="flex blog-post">
+            <div class="w-3/5 mx-auto">
+                <h1 class="text-3xl font-semibold">
+                    {{ $post->title }}
+                </h1>
 
-        {!! $post->content !!}
+                <p class="flex items-center">Escrito por <img src="{{ asset('images/jaimesares.jpg') }}" alt="" class="w-8 rounded-full mx-2"> Jaime Sares el {{ $post->published_at->format('d/m/Y') }}</p>
+
+                <div class="mb-8"></div>
+
+                {!! $post->content !!}
+            </div>
+        </div>
     </div>
 
 @endsection
