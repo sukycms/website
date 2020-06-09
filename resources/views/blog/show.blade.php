49eb5bbb6f982abb11869b4aa1a4c7e1f3a602a6
Jaime Sares <jaimesares@gmail.com>
2020-06-09 16:14:56 +0200

    wip

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -31,8 +31,8 @@
                     <h6 class="font-semibold italic mb-6">¡Comparte!</h6>
                     <div class="flex items-center justify-center">
                         <a href="https://www.facebook.com/sharer/sharer.php?u={{ route('blog.show', ['id' => $post->id, 'slug' => $post->getSlug()]) }}" target="_blank" class="px-4 py-2 rounded text-white mx-2" style="background-color: #4267B2;"><i class="fab fa-facebook-f fa-fw"></i></a>
-                        <a href="https://twitter.com/intent/tweet?text={{ route('blog.show', ['id' => $post->id, 'slug' => $post->getSlug()]) }}" target="_blank" class="px-4 py-2 rounded text-white mx-2" style="background-color: #25d366;"><i class="fab fa-whatsapp fa-fw"></i></a>
-                        <a href="whatsapp://send?text={{ route('blog.show', ['id' => $post->id, 'slug' => $post->getSlug()]) }}" target="_blank" class="px-4 py-2 rounded text-white mx-2" style="background-color: #55acee;"><i class="fab fa-twitter fa-fw"></i></a>
+                        <a href="whatsapp://send?text={{ route('blog.show', ['id' => $post->id, 'slug' => $post->getSlug()]) }}" target="_blank" class="px-4 py-2 rounded text-white mx-2" style="background-color: #25d366;"><i class="fab fa-whatsapp fa-fw"></i></a>
+                        <a href="https://twitter.com/intent/tweet?text={{ route('blog.show', ['id' => $post->id, 'slug' => $post->getSlug()]) }}" target="_blank" class="px-4 py-2 rounded text-white mx-2" style="background-color: #55acee;"><i class="fab fa-twitter fa-fw"></i></a>
                         <a href="mailto:?body={{ route('blog.show', ['id' => $post->id, 'slug' => $post->getSlug()]) }}" target="_blank" class="px-4 py-2 rounded text-white mx-2" style="background-color: #7d7d7d;"><i class="fas fa-envelope fa-fw"></i></a>
                     </div>
                 </div>
