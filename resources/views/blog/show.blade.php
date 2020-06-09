01723ee012210cdc377a4dfdf041e184567baa21
Jaime Sares <jaimesares@gmail.com>
2020-06-09 16:01:08 +0200

    wip

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -28,7 +28,7 @@
                 </div>
 
                 <div class="blog-post-social text-center mt-12">
-                    <h6 class="font-semibold italic">¡Comparte!</h6>
+                    <h6 class="font-semibold italic mb-6">¡Comparte!</h6>
                     <div class="flex items-center justify-center">
                         <a href="https://www.facebook.com/sharer/sharer.php?u={{ route('blog.show', ['id' => $post->id, 'slug' => $post->getSlug()]) }}" target="_blank" class="px-4 py-2 rounded text-white mx-2" style="background-color: #4267B2;"><i class="fab fa-facebook-f fa-fw"></i></a>
                         <a href="https://twitter.com/intent/tweet?text={{ route('blog.show', ['id' => $post->id, 'slug' => $post->getSlug()]) }}" target="_blank" class="px-4 py-2 rounded text-white mx-2" style="background-color: #25d366;"><i class="fab fa-whatsapp fa-fw"></i></a>
